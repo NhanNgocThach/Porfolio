889fc0816f55965ceed843fc259b55e032a9e35f
Ngọc Thạch Nhan <0n7n0t8@gmail.com>
2025-04-11 17:14:08 -0400

    update class.html

--- scripts/class-data.js
+++ scripts/class-data.js
@@ -1,37 +1,80 @@
 const classes = [
     {
+      id: "AE021",
+      name: "AE 021/031 – Listening and Speaking",
+      term: "Fall 2024",
+      start: "Sep 2024",
+      end: "Dec 2024",
+      projects: []
+    },
+    {
+      id: "AE023",
+      name: "AE 023/033 – Grammar and Vocabulary",
+      term: "Fall 2024",
+      start: "Sep 2024",
+      end: "Dec 2024",
+      projects: []
+    },
+    {
+      id: "AE024",
+      name: "AE 024/034 – American Cultural Studies",
+      term: "Fall 2024",
+      start: "Sep 2024",
+      end: "Dec 2024",
+      projects: []
+    },
+    {
+      id: "AE032",
+      name: "AE 032 – Reading and Writing Level 3",
+      term: "Fall 2024",
+      start: "Sep 2024",
+      end: "Dec 2024",
+      projects: []
+    },
+    {
+      id: "AE041",
+      name: "AE 041 – Listening and Speaking Level 4",
+      term: "Spring 2025",
+      start: "Jan 2025",
+      end: "Apr 2025",
+      projects: []
+    },
+    {
       id: "AE042",
-      name: "AE 042 – Writing II",
-      start: "Jan 2024",
-      end: "Mar 2024",
+      name: "AE 042 – Reading and Writing Level 4",
+      term: "Spring 2025",
+      start: "Jan 2025",
+      end: "Apr 2025",
       projects: [
         {
           type: "Essay",
           title: "Analysis Essay",
-          link: "https://nhanngocthach.github.io/Porfolio/files/ae042_analysis.pdf",
+          link: "./files/ae042_analysis.pdf",
           download: true
         },
         {
           type: "Essay",
           title: "Opinion Essay",
-          link: "https://nhanngocthach.github.io/Porfolio/files/ae042_opinion.pdf",
+          link: "./files/ae042_opinion.pdf",
           download: true
         }
       ]
     },
     {
       id: "AE043",
-      name: "AE 043 – Oral Communication",
-      start: "Jan 2024",
-      end: "Mar 2024",
-      projects: [
-        {
-          type: "Presentation",
-          title: "Final Speaking Task",
-          link: "https://nhanngocthach.github.io/Porfolio/files/ae043_presentation.pdf",
-          download: true
-        }
-      ]
+      name: "AE 043 – Student Engagement",
+      term: "Spring 2025",
+      start: "Jan 2025",
+      end: "Apr 2025",
+      projects: []
+    },
+    {
+      id: "AE044",
+      name: "AE 044 – American Culture Through Film",
+      term: "Spring 2025",
+      start: "Jan 2025",
+      end: "Apr 2025",
+      projects: []
     }
   ];
   
